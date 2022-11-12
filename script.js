30f144193237279303f1f2f26a82a794b16afb1c
DarSissau <dar.sissau@student.howest.be>
2022-11-12 18:24:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Dar Sissau
 // @match        https://www.facebook.com/pokes
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=facebook.com
 // @grant        none
